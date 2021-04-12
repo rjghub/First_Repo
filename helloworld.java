e1c403f8a1af420f8e9a2ac33c680fbd21c87387
rgjhub <ramy.jeg@gmail.com>
2021-04-12 07:04:34 -0400

    My message

---
+++ helloworld.java
@@ -0,0 +1,32 @@
+package Test1;
+
+public class helloworld {
+	
+	public static void main(String arg[]) {
+	method1();	
+	method2();	
+	method3();	
+	}
+	
+	
+	public static void method1() {
+		System.out.println("Hello World");
+	
+	}
+	
+	public static void method2() {
+	
+		String text="Good Day";
+		System.out.println(text);
+			
+	}
+	
+	public static String method3() {
+		
+		String text="Good Day";
+		System.out.println(text);
+		return text;
+	
+}
+	
+}
